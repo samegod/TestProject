0fdc9a843f6bd4abca2217165f7320f50a9f35aa
samegod <74413139+samegod@users.noreply.github.com>
2020-11-20 01:22:36 +0200

    add pause, lines and rotation

---
+++ TestProject/Assets/Scripts/DotScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotScript : MonoBehaviour
+{
+    public GameObject obj;
+    private Transform tr;
+
+    private float scale = 0.25f;
+    private bool agh = true;
+
+    private void Start()
+    {
+        obj = GetComponent<GameObject>();
+        tr = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (agh)
+        {
+            scale += 0.005f;
+            if (scale > 0.5f)
+                agh = false;
+        }
+        else
+        {
+            scale -= 0.005f;
+            if (scale < 0.25f)
+                agh = true;
+        }
+
+        tr.localScale = new Vector3(scale, scale, 0.1f);
+
+    }
+
+    IEnumerator destroy()
+    {
+        Debug.Log("Destroyed!");
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject, .5f);
+    }
+}
---
+++ TestProject/Assets/Scripts/Script.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public GameObject point;
+    public GameObject Character;
+    public float x = 200, y = 200;
+
+    public LinkedList<GameObject> dots = new LinkedList<GameObject>();
+
+    private Vector3 vec;
+    private Vector3 Path;
+    private Vector3 pos;
+
+    private bool Pause;
+    void Start()
+    {
+        vec = Character.transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Pause)
+            {
+                PauseGame();
+                Pause = false;
+            }
+            else
+            {
+                ResumeGame();
+                Pause = true;
+            }
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            vec = new Vector3((Input.mousePosition.x - Screen.width / 2) / 53, (Input.mousePosition.y - Screen.height / 2) / 53, 0);
+            dots.AddLast(Instantiate(point, vec, Quaternion.identity));
+
+        }
+
+        if (Mathf.Abs(Character.transform.position.x - pos.x) > 0.1 || Mathf.Abs(Character.transform.position.y - pos.y) > 0.1)
+        {
+            Character.transform.Translate(Path * Time.deltaTime);
+        }
+        else if (dots.First != null)
+        {
+            Debug.Log("Prepare to destroying!");
+
+            Path.x = dots.First.Value.transform.position.x - Character.transform.position.x;
+            Path.y = dots.First.Value.transform.position.y - Character.transform.position.y;
+
+            pos = dots.First.Value.transform.position;
+
+            dots.First.Value.SendMessage("destroy");
+            dots.RemoveFirst();
+        }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
